a45ea0400fbbda32480a431d589697c88f080b22
Mauro Lane <85325658+MauroLn26@users.noreply.github.com>
2024-11-04 22:37:45 -0300

    Segunda alteração

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
+import palavras_markin from './palavras_markin.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {<Exemplo></Exemplo>/* <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Olá, mundo!</code> and save to reload.
+          <code>Olá, mundo!</code> and save to reload.
         </p>
+        <br></br>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Learn Reac
+        </a> */}
       </header>
     </div>
   );
+}
+
+function Exemplo() {
+  return <div>
+    <img src={palavras_markin} className='Exemplo-palavras_markin' alt=""/>
+    <ul className='Exemplo-lista'>
+      <li>Inicio</li>
+      <li>Sobre</li>
+      <li>Contato</li>
+    </ul>
+  </div>
 }
 
 export default App;
